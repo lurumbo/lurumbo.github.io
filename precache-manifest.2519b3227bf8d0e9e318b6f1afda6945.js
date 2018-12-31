51a98305c4bf5787199eac4d119cb0625fe09001
Lu Rumbo <lurumbo18@gmail.com>
2018-12-31 16:06:01 -0300

    version 1.0

---
+++ precache-manifest.2519b3227bf8d0e9e318b6f1afda6945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f61b46d2b8e7d90529d9",
+    "url": "/static/js/main.f61b46d2.chunk.js"
+  },
+  {
+    "revision": "24f52bce6d6bb8fab40d",
+    "url": "/static/js/1.24f52bce.chunk.js"
+  },
+  {
+    "revision": "f61b46d2b8e7d90529d9",
+    "url": "/static/css/main.09ae4e07.chunk.css"
+  },
+  {
+    "revision": "3d51871f6b608081380cdbc2db9d9d91",
+    "url": "/index.html"
+  }
+];
